a4ed5641918abd676ad3836d21ae153b1aba5d21
sllamont <tyson.111.williams@gmail.com>
2024-04-10 12:30:46 -0400

    Configure register view/url

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,7 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path('register', registration, name='register')
+    path('register', views.registration, name='register'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
