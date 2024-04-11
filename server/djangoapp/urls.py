87b9c6e89620f1a1d269dfe9c9098c6da10a3343
sllamont <tyson.111.williams@gmail.com>
2024-04-11 16:06:01 -0400

    Fix typos

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -25,10 +25,10 @@ urlpatterns = [
     # path for get dealerships by state
     path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
 
-    # path for get dealer by id
+    # path for get dealer details
     path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
 
-    # path for dealer reviews view
+    # path for dealer reviews 
     path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -103,13 +103,12 @@ def get_dealer_reviews(request,dealer_id):
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
-        for review_detail in review:
+        for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
-            print(response)
             review_detail['sentiment'] = response['sentiment']
             return JsonResponse({"status":200,"reviews":reviews})
     else:
-        return JsonResponse({"Status":400,"message":"Bad Request"})
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 
 # Create a `get_dealer_details` view to render the dealer details
@@ -117,7 +116,7 @@ def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status":200,"dealers":dealership})
+        return JsonResponse({"status":200,"dealer":dealership})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 
